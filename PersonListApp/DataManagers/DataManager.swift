a93922af6aa8009cd8918b475239ad3374c7fb08
Alex Rybachev <rybachev.as@gmail.com>
2022-03-21 22:45:10 +0400

    add model and data manager

---
+++ PersonListApp/DataManagers/DataManager.swift
@@ -0,0 +1,67 @@
+//
+//  DataManager.swift
+//  PersonListApp
+//
+//  Created by Aleksandr Rybachev on 21.03.2022.
+//
+
+import Foundation
+
+struct DataManager {
+    
+    static let shared = DataManager()
+    
+    let firstNames = [
+        "John",
+        "Sharon",
+        "Aaron",
+        "Steven",
+        "Nicola",
+        "Ted",
+        "Bruce",
+        "Carl",
+        "Allan",
+        "Petr"
+    ]
+    
+    let lastNames = [
+        "Murphy",
+        "Jankin",
+        "Williams",
+        "Black",
+        "Robertson",
+        "Butler",
+        "Smith",
+        "Petrov",
+        "Ivanov",
+        "Sidorov"
+    ]
+    
+    let phones = [
+        "756295629",
+        "501285927",
+        "509298346",
+        "567392302",
+        "0129743411",
+        "1122334455",
+        "1234567890",
+        "0987654321",
+        "6543087193",
+        "0192837465"
+    ]
+    
+    let emails = [
+        "bob@ya.ru",
+        "petr@gmail.com",
+        "mir@mir.ru",
+        "gendalf@boss.org",
+        "abc@yard.ru",
+        "child@parent.ru",
+        "apple@apple.com",
+        "pentagon@usa.com",
+        "kreml@ru.ru",
+        "travel@world.org"
+    ]
+    
+//    init() {}
+}
---
+++ PersonListApp/Models/Person.swift
@@ -0,0 +1,40 @@
+//
+//  Person.swift
+//  PersonListApp
+//
+//  Created by Aleksandr Rybachev on 21.03.2022.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    let phone: String
+    let email: String
+    
+    var fullName: String {
+        "\(firstName) \(lastName)"
+    }
+    
+    static func getPersons() -> [Person] {
+        var persons: [Person] = []
+        
+        let firstNames = DataManager.shared.firstNames.shuffled()
+        let lastNames = DataManager.shared.lastNames.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        
+        for index in 0..<firstNames.count {
+            let person = Person(
+                firstName: firstNames[index],
+                lastName: lastNames[index],
+                phone: phones[index],
+                email: emails[index]
+            )
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
